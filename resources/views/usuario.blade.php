7a1fab8d5c8c441de14bac2f852a1ffef93c1221
Angel-Yahir-Castillo <yahirgamerpvz@gmail.com>
2023-01-28 22:03:31 -0600

    cerrar sesion

--- resources/views/usuario.blade.php
+++ resources/views/usuario.blade.php
@@ -9,7 +9,7 @@
 <div class="row">
     <div class="col s12 m3 l4"></div>
         <div class="col s12 m6 l4">
-            <img class="responsive-img" alt="Imagen de un telefono descompuesto" src="{{ asset('img/feliz.jpg') }}">
+            <img class="responsive-img" alt="Imagen de un telefono Feliz" src="{{ asset('img/feliz.jpg') }}">
         </div>
     <div class="col s12 m3 l4"></div>
 </div>
